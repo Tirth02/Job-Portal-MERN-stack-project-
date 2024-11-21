345782b19dc8317d003c9ea9e9b62be4ede2249a
Tirth Patel <pateltirth5442@gmail.com>
2024-11-21 22:02:11 +0530

    job controller and routes added

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import connectDB from "./utils/db.js";
 import dotenv from 'dotenv'
 import userRoute from "./routes/user.route.js"
 import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
 dotenv.config({});
 
 const app = express();
@@ -24,7 +25,7 @@ const PORT = process.env.PORT || 3000;
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
-
+app.use("/api/v1/job", jobRoute);
 
 app.listen(PORT, () =>{
     connectDB();
