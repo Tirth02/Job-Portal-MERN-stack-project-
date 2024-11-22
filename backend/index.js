e47e1cc14f9bf8fbeaf45ce7524999b4fe245134
Tirth Patel <pateltirth0902@gmail.com>
2024-11-23 00:18:12 +0530

    login and signup api binded and file setup with multer done in backend

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,9 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials:true
+    origin: 'http://127.0.0.1:5173',
+    credentials:true ,//access-control-allow-credentials:true
+    optionSuccessStatus:200
 }
 app.use(cors(corsOptions))
 const PORT = process.env.PORT || 3000;
--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -4,8 +4,10 @@ import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 import { Button } from "../ui/button";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { USER_API_END_POINT } from "@/constants/constant";
+import { toast } from "sonner";
+import axios from "axios";
 const Signup = () => {
   const [input, setInput] = useState({
     fullname: "",
@@ -15,7 +17,7 @@ const Signup = () => {
     role: "",
     file: "",
   });
-
+  const navigate = useNavigate();
   const changeEventHandler = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
   };
@@ -27,7 +29,33 @@ const Signup = () => {
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log(input);
+    const formData = new FormData();
+    formData.append("fullname",input.fullname);
+    formData.append("email",input.email);
+    formData.append("phoneNumber",input.phoneNumber);
+    formData.append("password",input.password);
+    formData.append("role",input.role);
+    if(input.file)
+    {
+        formData.append("file",input.file);
+    }
+    try {
+        const res = await axios.post(`${USER_API_END_POINT}/register`,formData,{
+            headers:{
+                "Content-Type":"multipart/form-data"
+            },
+            withCredentials:true
+        });
+        if(res.data.success)
+        {
+            navigate("/login");
+            toast.success(res.data.message);
+
+        }
+    } catch (error) {
+        console.log(error);
+        toast.error(error.response.data.message);
+    }
   };
   return (
     <div>
