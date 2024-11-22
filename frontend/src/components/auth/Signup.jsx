06ba6e701cbe53238bfc9332b0fa8b3d2937c123
Tirth Patel <pateltirth0902@gmail.com>
2024-11-22 16:21:45 +0530

    Signup and loginpage setup done

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,11 +1,131 @@
-import React from 'react'
+import React, { useState } from "react";
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [input, setInput] = useState({
+    fullname: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    role: "",
+    file: "",
+  });
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const changeFileHandler = (e) => {
+    console.log("Target files",e.target.files);
+    setInput({ ...input, file: e.target.files?.[0] });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(input);
+  };
   return (
     <div>
-      Signup
+      <Navbar />
+      <div className="flex items-center justify-center max-w-7xl mx-auto">
+        <form
+          onSubmit={submitHandler}
+          action=""
+          className="w-1/2 border boder-gray-200 rounded-md p-4 my-10"
+        >
+          <h1 className="font-bold text-xl mb-5">Sign up</h1>
+          <div className="my-2">
+            <Label>Full Name</Label>
+            <Input
+              type="text"
+              placeholder="e.g. John Doe"
+              value={input.fullname}
+              name="fullname"
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div className="my-2">
+            <Label>Email</Label>
+            <Input
+              type="email"
+              value={input.email}
+              name="email"
+              onChange={changeEventHandler}
+              placeholder="john@mail.com"
+            />
+          </div>
+          <div className="my-2">
+            <Label>Phone number</Label>
+            <Input
+              type="text"
+              placeholder="Phone Number"
+              value={input.phoneNumber}
+              name="phoneNumber"
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div className="my-2">
+            <Label>Password</Label>
+            <Input
+              type="password"
+              placeholder="Enter password"
+              value={input.password}
+              name="password"
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <RadioGroup className="flex items-center gap-4 my-5">
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  checked={input.role == "student"}
+                  onChange={changeEventHandler}
+                  value="student"
+                  className="cursor-pointer accent-black    "
+                />
+                <Label htmlFor="r1">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  checked={input.role == "recruiter"}
+                  onChange={changeEventHandler}
+                  value="recruiter"
+                  className="cursor-pointer accent-black"
+                />
+                <Label htmlFor="r2">Recruiter</Label>
+              </div>
+            </RadioGroup>
+            <div className="flex items-center gap-2">
+              <Label>Profile</Label>
+              <Input
+                accept="image/*"
+                type="file"
+                onChange={changeFileHandler}
+                className="cursor-pointer"
+              />
+            </div>
+          </div>
+          <Button type="submit" className="w-full my-4">
+            Signup
+          </Button>
+          <Link to="/login">
+            <span className="text-sm text-blue-600">
+              Already have an account? Login
+            </span>
+          </Link>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
