578ee73c361f4490c89262a05d93316454927555
Tirth Patel <pateltirth0902@gmail.com>
2024-12-07 12:12:47 +0530

    update issue solved and jobs extracted from backend

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,6 @@ app.use(cookieParser());
 const corsOptions = {
     origin: 'http://127.0.0.1:5173',
     credentials:true ,//access-control-allow-credentials:true
-    optionSuccessStatus:200
 }
 app.use(cors(corsOptions))
 const PORT = process.env.PORT || 3000;
