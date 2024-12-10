6fa55b5ab0d09d95e0046e95e5f429ac90028a53
Tirth Patel <pateltirth0902@gmail.com>
2024-12-10 23:56:25 +0530

    ui with api call for company registration and update done

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import { getAllCompany, getCompanyById, registerCompany, updateCompany } from "../controllers/company.controller.js";
+import { singleUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
 router.route("/register").post(isAuthenticated,registerCompany);
 router.route("/get").get(isAuthenticated,getAllCompany);
 router.route("/get/:id").get(isAuthenticated,getCompanyById);
-router.route("/update/:id").put(isAuthenticated,updateCompany);
+router.route("/update/:id").put(isAuthenticated,singleUpload,updateCompany);
 
 export default router;
